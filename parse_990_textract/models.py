83b08b7785abd023476087724efe5cfc7b240ba2
Ethan Corey <ethanscorey@gmail.com>
2022-09-09 22:20:39 -0400

    I hate typos

--- parse_990_textract/models.py
+++ parse_990_textract/models.py
@@ -212,7 +212,7 @@ class TableExtractor:
         col_spans = self.get_col_spans(words, page)
         columnized = columnize(word_clusters[0], col_spans)
         columnized.index = self.fields
-        last_col_coords = pd.DataFrame.from_records(
+        last_cluseter_coords = pd.DataFrame.from_records(
             columnized.map(get_cluster_coords)
         )
         rows = []
